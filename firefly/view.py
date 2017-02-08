22dc8bbcd3b78316851e7efc37078f877eb00567
Martastain <martas@imm.cz>
2017-02-08 19:53:49 +0100

    cell formats refactored

--- firefly/view.py
+++ firefly/view.py
@@ -1,7 +1,7 @@
 from .common import *
 from .widgets import *
 
-__all__ = ["format_header", "FireflyViewModel", "FireflySortModel", "FireflyView"]
+__all__ = ["FireflyViewModel", "FireflySortModel", "FireflyView"]
 
 def format_header(key):
     return meta_types[key].header(config.get("language", "en"))
@@ -12,13 +12,18 @@ class FireflyViewModel(QAbstractTableModel):
         self.object_data = []
         self.header_data = []
         self.changed_objects = []
-        self.font_normal  = QFont()
-        self.font_virtual = QFont()
-        self.font_virtual.setItalic(True)
-        self.font_bold = QFont()
-        self.font_bold.setBold(True)
-        self.font_underline = QFont()
-        self.font_underline.setUnderline(True)
+
+        font_italic = QFont()
+        font_italic.setItalic(True)
+        font_bold = QFont()
+        font_bold.setBold(True)
+        font_underline = QFont()
+        font_underline.setUnderline(True)
+        self.fonts = {
+                "bold" : font_bold,
+                "italic" : font_italic,
+                "underline" : font_underline
+           }
 
     def rowCount(self, parent):
         return len(self.object_data)
@@ -36,44 +41,29 @@ class FireflyViewModel(QAbstractTableModel):
             return None
         row = index.row()
         obj = self.object_data[row]
-        obj.model = self
         key = self.header_data[index.column()]
 
         if role == Qt.DisplayRole:
-            return obj.show(key)
-       # elif role == Qt.ForegroundRole:
-       #     return QColor(obj.format_foreground(tag))
-       # elif role == Qt.BackgroundRole:
-       #     bkg = obj.format_background(tag, self)
-       #     if bkg:
-       #         return QColor(bkg)
-       # elif role == Qt.EditRole:
-       #     return obj.format_edit(tag)
-       # elif role == Qt.UserRole:
-       #     return obj.format_sort(tag)
-       # elif role == Qt.DecorationRole:
-       #     return pixlib[obj.format_decoration(tag)]
-       # elif role == Qt.ToolTipRole:
-       #     return "\n".join(
-       #     "{} : {}".format(
-       #         meta_types.tag_alias(tag, config.get("language", "en-US")),
-       #         obj.format_display(tag)
-       #         ) for tag in sorted(obj.meta.keys()) if obj.format_display(tag)
-       #     ) if config.get("debug", False) else None
-       # elif role == Qt.FontRole:
-       #     if obj.object_type == "event":
-       #         return self.font_bold
-       #     elif obj.object_type == "item" and obj["id_asset"] == obj["rundown_event_asset"]:
-       #         return self.font_bold
-       #     else:
-       #         return self.font_normal
+            return obj.format_display(key, model=self)
+        elif role == Qt.ForegroundRole:
+            color = obj.format_foreground(key, model=self)
+            return QColor(color) if color else None
+        elif role == Qt.BackgroundRole:
+            color = obj.format_background(key, model=self)
+            return QColor(color) if color else None
+        elif role == Qt.DecorationRole:
+            return pix_lib[obj.format_decoration(key, model=self)]
+        elif role == Qt.FontRole:
+            font = obj.format_font(key, model=self)
+            if font in self.fonts:
+                return self.fonts[font]
         return None
 
 
     def setData(self, index, data, role=False):
-        tag = self.header_data[index.column()]
+        key = self.header_data[index.column()]
         id_object = self.object_data[index.row()].id
-        self.object_data[index.row()][tag] = data
+        self.object_data[index.row()][key] = data
 
         if not id_object in self.changed_objects:
             self.changed_objects.append(id_object)
@@ -94,7 +84,6 @@ class FireflySortModel(QSortFilterProxyModel):
         self.setSourceModel(model)
         self.setDynamicSortFilter(True)
         self.setSortLocaleAware(True)
-        #self.setSortRole(Qt.UserRole)
 
     @property
     def object_data(self):
