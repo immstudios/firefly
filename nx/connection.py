39ee10cfe2bf439d3d2a149dbf10f8995397d47c
Martastain <martas@imm.cz>
2018-06-07 15:38:53 +0200

    api uses nebularesponse

--- nx/connection.py
+++ nx/connection.py
@@ -13,40 +13,6 @@ headers = {
         'User-Agent': 'nebula-firefly/{}'.format(FIREFLY_VERSION),
     }
 
-
-class APIResult(object):
-    def __init__(self, **kwargs):
-        self._data = {}
-        self._data.update(kwargs)
-
-    @property
-    def response(self):
-        return self.get("response", 500)
-
-    @property
-    def message(self):
-        return self.get("message", "Invalid data")
-
-    @property
-    def data(self):
-        return self.get("data", {})
-
-    @property
-    def is_success(self):
-        return self.response < 400
-
-    @property
-    def is_error(self):
-        return self.response >= 400
-
-    def get(self, key, default=False):
-        return self._data.get(key, default)
-
-    def __getitem__(self, key):
-        return self._data[key]
-
-
-
 class NebulaAPI(object):
     def __init__(self, **kwargs):
         self._settings = kwargs
@@ -88,7 +54,7 @@ class NebulaAPI(object):
         response = requests.post(self._settings["hub"] + "/login", data, headers=headers)
         self._cookies = response.cookies
         data = json.loads(response.text)
-        return APIResult(**data)
+        return NebulaResponse(**data)
 
     def run(self, method, **kwargs):
         response = requests.post(
@@ -99,7 +65,7 @@ class NebulaAPI(object):
             )
         self._cookies = response.cookies
         if response.status_code >= 400:
-            return APIResult(
+            return NebulaResponse(
                     response=response.status_code,
                     message=DEFAULT_RESPONSE_MESSAGES.get(
                             response.status_code,
@@ -108,7 +74,7 @@ class NebulaAPI(object):
                 )
 
         data = json.loads(response.text)
-        return APIResult(**data)
+        return NebulaResponse(**data)
 
     def __getattr__(self, method_name):
         def wrapper(**kwargs):
