92d2c3f4b760ff33744ed4551423d30b3fe1e82f
Martastain <martas@imm.cz>
2018-08-15 00:04:45 +0200

    added region bar and new shortcuts

--- proxyplayer/videoplayer.py
+++ proxyplayer/videoplayer.py
@@ -30,6 +30,50 @@ class ToolBarStretcher(QWidget):
         self.setSizePolicy(QSizePolicy.Expanding,QSizePolicy.Expanding)
 
 
+class RegionBar(QWidget):
+    def __init__(self,parent):
+        super(RegionBar, self).__init__(parent)
+        self.marks_color = QColor("#009fbc")
+        self.in_color = QColor("#009f00")
+        self.out_color = QColor("#9f0000")
+        self.setFixedHeight(6)
+        self.show()
+
+    def paintEvent(self, event=False):
+        qp = QPainter()
+        qp.begin(self)
+        self.drawRegion(qp)
+        qp.end()
+
+    @property
+    def duration(self):
+        return self.parent().duration
+
+    @property
+    def mark_in(self):
+        return self.parent().mark_in
+
+    @property
+    def mark_out(self):
+        return self.parent().mark_out or self.parent().duration
+
+    def drawRegion(self, qp):
+        if not self.duration:
+            return
+        w = self.width()
+        h = self.height()
+        x1 = (float(w) / self.duration) * (self.mark_in)
+        x2 = (float(w) / self.duration) * (self.mark_out - self.mark_in)
+        qp.setPen(Qt.NoPen)
+        qp.setBrush(self.marks_color)
+        qp.drawRect(x1, 1, x2, h-4)
+
+        qp.setBrush(self.in_color)
+        qp.drawRect(x1,      0, 2, 3)
+        qp.setBrush(self.out_color)
+        qp.drawRect(x1+x2  , 3, 2, 3)
+
+
 def get_navbar(wnd):
     toolbar = QToolBar(wnd)
 
@@ -50,7 +94,7 @@ def get_navbar(wnd):
     toolbar.addAction(wnd.action_clear_in)
 
     wnd.action_mark_in = QAction(QIcon(wnd.pixlib["mark-in"]), "Mark in", wnd)
-    wnd.action_mark_in.setShortcut('e')
+    wnd.action_mark_in.setShortcuts(['e', 'i'])
     wnd.action_mark_in.setStatusTip('Mark in')
     wnd.action_mark_in.triggered.connect(wnd.on_mark_in)
     toolbar.addAction(wnd.action_mark_in)
@@ -63,46 +107,58 @@ def get_navbar(wnd):
 
     toolbar.addWidget(ToolBarStretcher(wnd))
 
+    wnd.action_go_start = QAction(QIcon(wnd.pixlib["fast-backward"]), 'Go to start', wnd)
+    wnd.action_go_start.setShortcuts(['a', 'home'])
+    wnd.action_go_start.triggered.connect(wnd.on_go_start)
+    wnd.addAction(wnd.action_go_start)
+    toolbar.addAction(wnd.action_go_start)
+
     wnd.action_frame_prev5 = QAction(QIcon(wnd.pixlib["previous-more"]), 'Previous 5 frames', wnd)
-    wnd.action_frame_prev5.setShortcut('1')
+    wnd.action_frame_prev5.setShortcuts(['1', 'j'])
     wnd.action_frame_prev5.triggered.connect(wnd.on_5_prev)
     wnd.addAction(wnd.action_frame_prev5)
     toolbar.addAction(wnd.action_frame_prev5)
 
     wnd.action_frame_prev = QAction(QIcon(wnd.pixlib["previous"]), "Previous frame", wnd)
-    wnd.action_frame_prev.setShortcut('3')
+    wnd.action_frame_prev.setShortcuts(['3', 'Left'])
     wnd.action_frame_prev.setStatusTip('Go to previous frame')
     wnd.action_frame_prev.triggered.connect(wnd.on_frame_prev)
     toolbar.addAction(wnd.action_frame_prev)
 
     wnd.action_play = QAction(QIcon(wnd.pixlib["play"]), "Play", wnd)
-    wnd.action_play.setShortcut('Space')
+    wnd.action_play.setShortcuts(['Space', 'k'])
     wnd.action_play.setStatusTip('Play/Pause')
     wnd.action_play.triggered.connect(wnd.on_pause)
     toolbar.addAction(wnd.action_play)
 
     wnd.action_frame_next = QAction(QIcon(wnd.pixlib["next"]), "Next frame", wnd)
-    wnd.action_frame_next.setShortcut('4')
+    wnd.action_frame_next.setShortcuts(['4', 'Right'])
     wnd.action_frame_next.setStatusTip('Go to next frame')
     wnd.action_frame_next.triggered.connect(wnd.on_frame_next)
     toolbar.addAction(wnd.action_frame_next)
 
     wnd.action_frame_next5 = QAction(QIcon(wnd.pixlib["next-more"]), 'Next 5 frames', wnd)
-    wnd.action_frame_next5.setShortcut('2')
+    wnd.action_frame_next5.setShortcuts(['2', 'l'])
     wnd.action_frame_next5.triggered.connect(wnd.on_5_next)
     wnd.addAction(wnd.action_frame_next5)
     toolbar.addAction(wnd.action_frame_next5)
 
+    wnd.action_go_end = QAction(QIcon(wnd.pixlib["fast-forward"]), 'Go to end', wnd)
+    wnd.action_go_end.setShortcuts(['s', 'end'])
+    wnd.action_go_end.triggered.connect(wnd.on_go_end)
+    wnd.addAction(wnd.action_go_end)
+    toolbar.addAction(wnd.action_go_end)
+
     toolbar.addWidget(ToolBarStretcher(wnd))
 
     wnd.action_goto_out = QAction(QIcon(wnd.pixlib["goto-out"]), "Go to out", wnd)
-    wnd.action_goto_out.setShortcut('W')
+    wnd.action_goto_out.setShortcut('w')
     wnd.action_goto_out.setStatusTip('Go to selection end')
     wnd.action_goto_out.triggered.connect(wnd.on_go_out)
     toolbar.addAction(wnd.action_goto_out)
 
     wnd.action_mark_out = QAction(QIcon(wnd.pixlib["mark-out"]), "Mark out", wnd)
-    wnd.action_mark_out.setShortcut('R')
+    wnd.action_mark_out.setShortcuts(['r', 'o'])
     wnd.action_mark_out.setStatusTip('Mark out')
     wnd.action_mark_out.triggered.connect(wnd.on_mark_out)
     toolbar.addAction(wnd.action_mark_out)
@@ -181,7 +237,7 @@ class VideoPlayer(QWidget):
         self.timeline = QSlider(Qt.Horizontal)
         self.timeline.setRange(0, 0)
         self.timeline.sliderMoved.connect(self.on_timeline_seek)
-
+        self.region_bar = RegionBar(self)
         self.navbar = get_navbar(self)
 
         #
@@ -206,6 +262,7 @@ class VideoPlayer(QWidget):
         layout = QVBoxLayout()
         layout.addLayout(top_bar)
         layout.addWidget(self.video_window)
+        layout.addWidget(self.region_bar)
         layout.addWidget(self.timeline)
         layout.addLayout(bottom_bar)
 
@@ -239,6 +296,7 @@ class VideoPlayer(QWidget):
         else:
             self.duration = 0
             self.mark_out = 0
+        self.region_bar.update()
 
     def on_timeline_seek(self):
         self.player["pause"] = True
@@ -256,6 +314,12 @@ class VideoPlayer(QWidget):
     def on_5_prev(self):
         self.player.seek(-5*self.frame_dur, "relative", "exact")
 
+    def on_go_start(self):
+        self.player.seek(0, "absolute", "exact")
+
+    def on_go_end(self):
+        self.player.seek(self.duration, "absolute", "exact")
+
     def on_go_in(self):
         self.seek(self.mark_in)
 
@@ -264,15 +328,19 @@ class VideoPlayer(QWidget):
 
     def on_mark_in(self):
         self.mark_in = self.position
+        self.region_bar.update()
 
     def on_mark_out(self):
         self.mark_out = self.position
+        self.region_bar.update()
 
     def on_clear_in(self):
         self.mark_in = 0
+        self.region_bar.update()
 
     def on_clear_out(self):
         self.mark_out = self.duration
+        self.region_bar.update()
 
     def seek(self, position):
         self.player.seek(position, "absolute", "exact")
